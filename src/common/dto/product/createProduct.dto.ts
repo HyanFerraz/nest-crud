fe7c31654a60cf53eeaf4364cabc0b177736f65c
Hyan Ferraz <hyanfr01@gmail.com>
2024-04-06 18:55:01 -0300

    [feat] : Implement validation in Products service

--- src/common/dto/product/createProduct.dto.ts
+++ src/common/dto/product/createProduct.dto.ts
@@ -1,7 +1,18 @@
+import { IsNotEmpty, IsNumberString } from 'class-validator';
+
 export class CreateProductDto {
+  @IsNotEmpty()
   name: string;
+
+  @IsNotEmpty()
   size: string;
+
+  @IsNotEmpty()
   color: string;
+
+  @IsNumberString()
   price: number;
+
+  @IsNumberString()
   quantity: number;
 }
